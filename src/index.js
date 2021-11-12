1744797014f919ba514541f8a7146206a195cc96
William Martins <williammds00@gmail.com>
2021-11-12 19:16:34 -0300

    chore: add app folder with controllers, middlewares and models

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/authController')(app);
+require('./app/controllers/projectController')(app);
 
 app.get('/', (req, res) => 
 res.send('Server working'));
