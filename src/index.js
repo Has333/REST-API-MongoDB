e89ee3dddbf5bd152a5008c645e3992cd4594533
William Martins <williammds00@gmail.com>
2021-11-12 22:27:31 -0300

    feat: add password recovery system

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
 app.get('/', (req, res) => 
 res.send('Server working'));
